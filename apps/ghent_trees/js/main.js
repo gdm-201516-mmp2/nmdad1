3f5b2fde9fcf89fa4a76daeaea6e90d60ad2ac27
drdynscript <drdynscript@gmail.com>
2015-11-10 14:17:29 +0100

    Add Tree App

--- apps/ghent_trees/js/main.js
+++ apps/ghent_trees/js/main.js
@@ -28,10 +28,26 @@
 				function(data) {
 					var treesInventoryOriginal = data.Document.Folder.Placemark;
 					var l = 50;//max 4 MB in localstorage
-					var treeOriginal = null;
+					var treeOriginal = null, tree = null;
 					for(var i = 0; i < l; i++) {
 						treeOriginal = treesInventoryOriginal[i];// Get the current tree in the array by index
-						self._treesDbContext.addTreeToInventory(treeOriginal);// Add tree to localstorage
+						tree = {
+							"ghentid": treeOriginal.ExtendedData.SchemaData.SimpleData[1]['@text'],
+							"ghentcode": treeOriginal.ExtendedData.SchemaData.SimpleData[2]['@text'],
+							"name": treeOriginal.ExtendedData.SchemaData.SimpleData[3]['@text'],
+							"namenl": treeOriginal.ExtendedData.SchemaData.SimpleData[4]['@text'],
+							"dimensions": {
+								"height": treeOriginal.ExtendedData.SchemaData.SimpleData[5]['@text'],
+								"diameter": treeOriginal.ExtendedData.SchemaData.SimpleData[6]['@text']
+							},
+							"dayofbirth": parseInt(treeOriginal.ExtendedData.SchemaData.SimpleData[7]['@text']),
+							"geolocation": {
+								"lat": parseFloat(treeOriginal.Point.coordinates['@text'].split(',')[1]),
+								"lng": parseFloat(treeOriginal.Point.coordinates['@text'].split(',')[0])
+							}
+						};
+						console.log(tree);
+						//self._treesDbContext.addTreeToInventory(tree);// Add tree to localstorage
 					}
 				},
 				function(error) {
