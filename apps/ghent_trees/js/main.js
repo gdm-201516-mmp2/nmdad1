8580a3732e589abbaf48b7af2a135cc2a6e5945b
drdynscript <drdynscript@gmail.com>
2015-11-10 12:58:56 +0100

    Add trees app

---
+++ apps/ghent_trees/js/main.js
@@ -0,0 +1,15 @@
+/*
+*	Description: Ghent Trees Application
+*	Modified: 10-11-2015
+*	Version: 1.0.0
+*	Author: Philippe De Pauw - Waterschoot
+* 	-----------------------------------------------
+*	Ecoplan API: http://datatank.stad.gent/4/milieuennatuur/ecoplan.json
+*/
+
+// Anonymous function executed when document loaded
+(function() {
+	
+	
+	
+})();
