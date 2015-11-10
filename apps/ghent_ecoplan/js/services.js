c67d80802cb8fb54d73e109058c13755088d8a34
drdynscript <drdynscript@gmail.com>
2015-11-10 09:51:08 +0100

    Update Ecoplan App

--- apps/ghent_ecoplan/js/services.js
+++ apps/ghent_ecoplan/js/services.js
@@ -12,3 +12,24 @@ var EcoplanAPI = {
 		return Utils.getJSONByPromise(url);
 	}
 }
+
+/*
+* DbContext for Ecoplan
+* CRUD operations
+*/
+var EcoplanDbContext = {
+	init: function(connString) {
+		this._connString = connString;// Connection string to the localstorage
+		// Create an initial object --> save later
+		this._appData = {
+			"information": {
+				"title": "Ghent Ecoplan Application",
+				"modified": new Date(),
+				"version": "1.0.0",
+				"author": "Philippe De Pauw - Waterschoot"
+			},
+			
+			"settings": {}	
+		};
+	}
+}
