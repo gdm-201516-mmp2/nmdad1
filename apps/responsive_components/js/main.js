5936ec06dcd7a24cc2d1721a468a2cdbbbe335e8
drdynscript <drdynscript@gmail.com>
2015-11-17 11:40:20 +0100

    Update Responsive Components

--- apps/responsive_components/js/main.js
+++ apps/responsive_components/js/main.js
@@ -13,7 +13,26 @@
 	// Describe an App object with own functionalities
 	var App = {
 		init: function() {
+			this.registerNavigationToggleListeners();// Register All Navigation Toggle Listeners
+		},
+		registerNavigationToggleListeners: function() {
+			var toggles = document.querySelectorAll('.header__navigation-toggle');
 			
+			if(toggles != null && toggles.length > 0) {
+				var toggle = null, navid = null;
+				
+				for(var i = 0; i < toggles.length; i++ ) {
+					toggle = toggles[i];
+					toggle.addEventListener('click', function(ev) {
+						ev.preventDefault();
+						
+						navid = this.dataset.navid;
+						document.querySelector('#' + navid).classList.toggle('open');
+						
+						return false;
+					});	
+				}
+			}
 		}
 	};
 	
