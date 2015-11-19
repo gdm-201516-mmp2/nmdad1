3850643c902149386c91b449fc4827e79437c8cc
drdynscript <drdynscript@gmail.com>
2015-11-19 11:33:00 +0100

    Add Worldbank app

---
+++ apps/worldbank/js/jaywalker.js
@@ -0,0 +1,56 @@
+/*
+* Description: JayWalker
+* Modified: 17-11-2015
+* Version: 1.0.0
+* Author: Philippe De Pauw - Waterschoot
+*/
+var JayWalker = {
+	init: function() {
+		this._router = crossroads;// Clone the crossroads object
+		this._hasher = hasher;// Clone the hasher object
+		this.initRouter();// Initialize the router and hasher		
+	},
+	initRouter: function() {
+		var self = this;
+		
+		// Router settings: Register routes
+		var homeRoute = this._router.addRoute('/', function() {
+			self.setActivePage('home');
+		});
+		var sectionRoute = this._router.addRoute('/{section}');// Add section route to the router (crossroads)
+		sectionRoute.matched.add(function(section) {
+			self.setActivePage(section);
+		});
+		
+		//Hasher settings
+		this._hasher.initialized.add(function(newHash, oldHash) {
+			self._router.parse(newHash);
+		});// Parse initial hash
+		this._hasher.changed.add(function(newHash, oldHash) {
+			self._router.parse(newHash);
+			if(document.querySelector('body').classList.contains('offcanvas-open')) {
+				document.querySelector('body').classList.remove('offcanvas-open');
+			}
+			
+			if(document.querySelector('body').classList.contains('headernav-open')) {
+				document.querySelector('body').classList.remove('headernav-open');
+			}
+		});// Parse changes in the hash
+		this._hasher.init();// Start listening to the hashes
+	},
+	setActivePage: function(pageId) {
+		var pages = document.querySelectorAll('.page');
+		if(pages != null && pages.length > 0) {
+			var page = null;
+			for(var i = 0; i < pages.length;i++) {
+				page = pages[i];
+				
+				if(page.id == pageId) {
+					page.classList.add('active');
+				} else {
+					page.classList.remove('active');
+				}
+			}
+		}
+	}	
+};
---
+++ apps/worldbank/js/main.js
@@ -0,0 +1,59 @@
+/*
+*	Description: Worldbank
+*	Modified: 10-11-2015
+*	Version: 1.0.0
+*	Author: Philippe De Pauw - Waterschoot
+* 	-----------------------------------------------
+*	Worldbank countries API: http://api.worldbank.org/countries/all?format=jsonP&prefix=jsonp_callback&per_page=300
+* 
+*   ?([{"page":1,"pages":1,"per_page":"300","total":264},[{"id":"ABW","iso2Code":"AW","name":"Aruba","region":{"id":"LCN","value":"Latin America & Caribbean (all income levels)"},"adminregion":{"id":"","value":""},"incomeLevel":{"id":"NOC","value":"High income: nonOECD"},"lendingType":{"id":"LNX","value":"Not classified"},"capitalCity":"Oranjestad","longitude":"-70.0167","latitude":"12.5167"}
+*/
+
+// Anonymous function executed when document loaded
+(function() {
+	
+	// Describe an App object with own functionalities
+	var App = {
+		init: function() {
+			this.WBCOUNTRIESAPIURL = "http://api.worldbank.org/countries/all?format=jsonP&prefix=jsonp_callback&per_page=300";
+			this._dataCountries = null;// Variable for the list of countries
+			
+			// Handlebars Cache
+			this._hbsCache = {};// Handlebars cache for templates
+			this._hbsPartialsCache = {};// Handlebars cache for partials
+			
+			this.loadCountriesFromWorldBankAPI();// Execute method loadCountriesFromWorldBankAPI(): Load countries from the Worldbank API
+		},
+		loadCountriesFromWorldBankAPI: function() {
+			// Closure
+			var self = this;
+			
+			// Load JSONP from corresponding API with certain URL
+			// JSONP Callback is defined by a function name in this case
+			// prefix=jsonp_callback. The Utils object contains a new function
+			// which can handle the callback
+			Utils.getJSONPByPromise(this.WBCOUNTRIESAPIURL).then(
+				function(data) {
+					if(data != null) {
+						self._dataCountries = data[1];// Assign data as value flor global variable _dataCountries within the App
+						console.log(self._dataCountries);
+						self.updateCountriesUI();// Call updateCountriesUI method when successful*/	
+					}	
+				},
+				function(status) {
+					console.log(status);
+				}
+			);
+		},
+		updateCountriesUI: function() {
+			if(!this._hbsCache['countries-list']) {
+				var src = document.querySelector('#countries-list-template').innerHTML;// Get the contents from the specified hbs template
+				this._hbsCache['countries-list'] = Handlebars.compile(src);// Compile the source and add it to the hbs cache
+			}	
+			document.querySelector('.countries-list').innerHTML = this._hbsCache['countries-list'](this._dataCountries);
+		}
+	};
+	
+	App.init();// Intialize the application
+	
+})();
